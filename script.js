5fd23c9c0258e8c1c731cbf40d1c84403dfa7530
Abhiraj028 <abhirajgautam028@gmail.com>
2025-03-01 17:22:55 +0530

    function version

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const container = document.querySelector(".container");
-
+let flag;
 createGrid(16);
 
 const input = document.querySelector(".input");
-input.addEventListener("click", () => {
+input.addEventListener("input", () => {
   createGrid(input.value);
 });
 
 function createGrid(range){
     container.innerHTML = "";
+    flag = "black";
+
+    const size = document.querySelector(".num");
+    size.innerText = range;
+
     for (let i = 0; i < range; i++) {
         const child = document.createElement("div");
         child.setAttribute("class", "head");
@@ -21,11 +26,35 @@ function createGrid(range){
           child.appendChild(follow);
         }
       }
+      paint(0,255);
 }
 
+function getRandomBetween(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-document.querySelectorAll(".box").forEach((box) => {
-  box.addEventListener("mouseover", () => {
-    box.classList.add("red");
-  });
+const reset = document.querySelector(".reset");
+reset.addEventListener("click",() => {
+  const box = document.querySelectorAll(".box").forEach((box) => {
+    box.setAttribute("style",`background-color: rgb(${0},${0},${0});`);
+  })
+  
 });
+
+const color = document.querySelector(".template");
+color.addEventListener("click",() => {
+  flag = "white";
+  const box = document.querySelectorAll(".box").forEach((box) => {
+    box.setAttribute("style",`background-color: rgb(${255},${255},${255});`);
+  })
+  
+})
+
+
+function paint(min,max){
+  document.querySelectorAll(".box").forEach((box) => {
+    box.addEventListener("mouseover", (min,max) => {
+      box.setAttribute('style', `background-color: rgb(${getRandomBetween(0,255)},${getRandomBetween(0,255)},${getRandomBetween(0,255)});`);
+    });
+  });
+}
